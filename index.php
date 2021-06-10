ab431c171fb5ecf0f5ec092e6c8e1458211dabd0
ihab abadi <ihab@utopios.net>
2021-06-10 14:56:28 +0200

    test2

--- index.php
+++ index.php
@@ -3,3 +3,4 @@ include_once 'header.php';
 echo "Hello from ou first git project on v1-devbis and test";
 echo "Hello from ou first git project on dev and master branch";
 echo "test";
+echo "test2";
